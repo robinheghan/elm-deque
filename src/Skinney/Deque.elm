cd04d5b8768f8c210ab46f6c0a39d6fa9e1bcf15
Robin Heggelund Hansen <robin.heggelund@icloud.com>
2019-04-06 15:49:44 +0200

    Optimize length

--- src/Skinney/Deque.elm
+++ src/Skinney/Deque.elm
@@ -22,7 +22,7 @@ module Skinney.Deque exposing
 type Deque a
     = Empty
     | Single a
-    | Deque (Buffer a) (Deque (Buffer a)) (Buffer a)
+    | Deque Int (Buffer a) (Deque (Buffer a)) (Buffer a)
 
 
 type Buffer a
@@ -56,28 +56,28 @@ pushFront element deque =
             Single element
 
         Single e1 ->
-            Deque (One element) Empty (One e1)
+            Deque 2 (One element) Empty (One e1)
 
-        Deque (One e1) middle end ->
-            Deque (Two element e1) middle end
+        Deque len (One e1) middle end ->
+            Deque (len + 1) (Two element e1) middle end
 
-        Deque (Two e1 e2) middle end ->
-            Deque (Three element e1 e2) middle end
+        Deque len (Two e1 e2) middle end ->
+            Deque (len + 1) (Three element e1 e2) middle end
 
-        Deque (Three e1 e2 e3) middle end ->
-            Deque (Four element e1 e2 e3) middle end
+        Deque len (Three e1 e2 e3) middle end ->
+            Deque (len + 1) (Four element e1 e2 e3) middle end
 
-        Deque (Four e1 e2 e3 e4) middle end ->
-            Deque (Five element e1 e2 e3 e4) middle end
+        Deque len (Four e1 e2 e3 e4) middle end ->
+            Deque (len + 1) (Five element e1 e2 e3 e4) middle end
 
-        Deque (Five e1 e2 e3 e4 e5) middle end ->
-            Deque (Six element e1 e2 e3 e4 e5) middle end
+        Deque len (Five e1 e2 e3 e4 e5) middle end ->
+            Deque (len + 1) (Six element e1 e2 e3 e4 e5) middle end
 
-        Deque (Six e1 e2 e3 e4 e5 e6) Empty (One s1) ->
-            Deque (Four element e1 e2 e3) Empty (Four e4 e5 e6 s1)
+        Deque len (Six e1 e2 e3 e4 e5 e6) Empty (One s1) ->
+            Deque (len + 1) (Four element e1 e2 e3) Empty (Four e4 e5 e6 s1)
 
-        Deque (Six e1 e2 e3 e4 e5 e6) middle end ->
-            Deque (Two element e1) (pushBufferFront (Five e2 e3 e4 e5 e6) middle) end
+        Deque len (Six e1 e2 e3 e4 e5 e6) middle end ->
+            Deque (len + 1) (Two element e1) (pushBufferFront (Five e2 e3 e4 e5 e6) middle) end
 
 
 pushBufferFront : Buffer a -> Deque (Buffer a) -> Deque (Buffer a)
@@ -92,28 +92,28 @@ pushBack element deque =
             Single element
 
         Single e1 ->
-            Deque (One e1) Empty (One element)
+            Deque 2 (One e1) Empty (One element)
 
-        Deque beginning middle (One e1) ->
-            Deque beginning middle (Two e1 element)
+        Deque len beginning middle (One e1) ->
+            Deque (len + 1) beginning middle (Two e1 element)
 
-        Deque beginning middle (Two e1 e2) ->
-            Deque beginning middle (Three e1 e2 element)
+        Deque len beginning middle (Two e1 e2) ->
+            Deque (len + 1) beginning middle (Three e1 e2 element)
 
-        Deque beginning middle (Three e1 e2 e3) ->
-            Deque beginning middle (Four e1 e2 e3 element)
+        Deque len beginning middle (Three e1 e2 e3) ->
+            Deque (len + 1) beginning middle (Four e1 e2 e3 element)
 
-        Deque beginning middle (Four e1 e2 e3 e4) ->
-            Deque beginning middle (Five e1 e2 e3 e4 element)
+        Deque len beginning middle (Four e1 e2 e3 e4) ->
+            Deque (len + 1) beginning middle (Five e1 e2 e3 e4 element)
 
-        Deque beginning middle (Five e1 e2 e3 e4 e5) ->
-            Deque beginning middle (Six e1 e2 e3 e4 e5 element)
+        Deque len beginning middle (Five e1 e2 e3 e4 e5) ->
+            Deque (len + 1) beginning middle (Six e1 e2 e3 e4 e5 element)
 
-        Deque (One p1) Empty (Six e1 e2 e3 e4 e5 e6) ->
-            Deque (Four p1 e1 e2 e3) Empty (Four e4 e5 e6 element)
+        Deque len (One p1) Empty (Six e1 e2 e3 e4 e5 e6) ->
+            Deque (len + 1) (Four p1 e1 e2 e3) Empty (Four e4 e5 e6 element)
 
-        Deque beginning middle (Six e1 e2 e3 e4 e5 e6) ->
-            Deque beginning (pushBufferBack (Five e1 e2 e3 e4 e5) middle) (Two e6 element)
+        Deque len beginning middle (Six e1 e2 e3 e4 e5 e6) ->
+            Deque (len + 1) beginning (pushBufferBack (Five e1 e2 e3 e4 e5) middle) (Two e6 element)
 
 
 pushBufferBack : Buffer a -> Deque (Buffer a) -> Deque (Buffer a)
@@ -130,40 +130,40 @@ popFront deque =
         Single e1 ->
             ( Just e1, Empty )
 
-        Deque (Six e1 e2 e3 e4 e5 e6) middle end ->
-            ( Just e1, Deque (Five e2 e3 e4 e5 e6) middle end )
+        Deque len (Six e1 e2 e3 e4 e5 e6) middle end ->
+            ( Just e1, Deque (len - 1) (Five e2 e3 e4 e5 e6) middle end )
 
-        Deque (Five e1 e2 e3 e4 e5) middle end ->
-            ( Just e1, Deque (Four e2 e3 e4 e5) middle end )
+        Deque len (Five e1 e2 e3 e4 e5) middle end ->
+            ( Just e1, Deque (len - 1) (Four e2 e3 e4 e5) middle end )
 
-        Deque (Four e1 e2 e3 e4) middle end ->
-            ( Just e1, Deque (Three e2 e3 e4) middle end )
+        Deque len (Four e1 e2 e3 e4) middle end ->
+            ( Just e1, Deque (len - 1) (Three e2 e3 e4) middle end )
 
-        Deque (Three e1 e2 e3) middle end ->
-            ( Just e1, Deque (Two e2 e3) middle end )
+        Deque len (Three e1 e2 e3) middle end ->
+            ( Just e1, Deque (len - 1) (Two e2 e3) middle end )
 
-        Deque (Two e1 e2) middle end ->
-            ( Just e1, Deque (One e2) middle end )
+        Deque len (Two e1 e2) middle end ->
+            ( Just e1, Deque (len - 1) (One e2) middle end )
 
-        Deque (One e1) Empty (One s1) ->
+        Deque len (One e1) Empty (One s1) ->
             ( Just e1, Single s1 )
 
-        Deque (One e1) Empty (Two s1 s2) ->
-            ( Just e1, Deque (One s1) Empty (One s2) )
+        Deque len (One e1) Empty (Two s1 s2) ->
+            ( Just e1, Deque (len - 1) (One s1) Empty (One s2) )
 
-        Deque (One e1) Empty (Three s1 s2 s3) ->
-            ( Just e1, Deque (One s1) Empty (Two s2 s3) )
+        Deque len (One e1) Empty (Three s1 s2 s3) ->
+            ( Just e1, Deque (len - 1) (One s1) Empty (Two s2 s3) )
 
-        Deque (One e1) Empty (Four s1 s2 s3 s4) ->
-            ( Just e1, Deque (Two s1 s2) Empty (Two s3 s4) )
+        Deque len (One e1) Empty (Four s1 s2 s3 s4) ->
+            ( Just e1, Deque (len - 1) (Two s1 s2) Empty (Two s3 s4) )
 
-        Deque (One e1) Empty (Five s1 s2 s3 s4 s5) ->
-            ( Just e1, Deque (One s1) Empty (Four s2 s3 s4 s5) )
+        Deque len (One e1) Empty (Five s1 s2 s3 s4 s5) ->
+            ( Just e1, Deque (len - 1) (One s1) Empty (Four s2 s3 s4 s5) )
 
-        Deque (One e1) Empty (Six s1 s2 s3 s4 s5 s6) ->
-            ( Just e1, Deque (Three s1 s2 s3) Empty (Three s4 s5 s6) )
+        Deque len (One e1) Empty (Six s1 s2 s3 s4 s5 s6) ->
+            ( Just e1, Deque (len - 1) (Three s1 s2 s3) Empty (Three s4 s5 s6) )
 
-        Deque (One e1) middle end ->
+        Deque len (One e1) middle end ->
             let
                 ( newFirst, newMiddle ) =
                     popBufferFront middle
@@ -174,7 +174,7 @@ popFront deque =
                     ( Nothing, Empty )
 
                 Just val ->
-                    ( Just e1, Deque val newMiddle end )
+                    ( Just e1, Deque (len - 1) val newMiddle end )
 
 
 popBufferFront : Deque (Buffer a) -> ( Maybe (Buffer a), Deque (Buffer a) )
@@ -191,40 +191,40 @@ popBack deque =
         Single e1 ->
             ( Just e1, Empty )
 
-        Deque beginning middle (Six e1 e2 e3 e4 e5 e6) ->
-            ( Just e6, Deque beginning middle (Five e1 e2 e3 e4 e5) )
+        Deque len beginning middle (Six e1 e2 e3 e4 e5 e6) ->
+            ( Just e6, Deque (len - 1) beginning middle (Five e1 e2 e3 e4 e5) )
 
-        Deque beginning middle (Five e1 e2 e3 e4 e5) ->
-            ( Just e5, Deque beginning middle (Four e1 e2 e3 e4) )
+        Deque len beginning middle (Five e1 e2 e3 e4 e5) ->
+            ( Just e5, Deque (len - 1) beginning middle (Four e1 e2 e3 e4) )
 
-        Deque beginning middle (Four e1 e2 e3 e4) ->
-            ( Just e4, Deque beginning middle (Three e1 e2 e3) )
+        Deque len beginning middle (Four e1 e2 e3 e4) ->
+            ( Just e4, Deque (len - 1) beginning middle (Three e1 e2 e3) )
 
-        Deque beginning middle (Three e1 e2 e3) ->
-            ( Just e3, Deque beginning middle (Two e1 e2) )
+        Deque len beginning middle (Three e1 e2 e3) ->
+            ( Just e3, Deque (len - 1) beginning middle (Two e1 e2) )
 
-        Deque beginning middle (Two e1 e2) ->
-            ( Just e2, Deque beginning middle (One e1) )
+        Deque len beginning middle (Two e1 e2) ->
+            ( Just e2, Deque (len - 1) beginning middle (One e1) )
 
-        Deque (One p1) Empty (One e1) ->
+        Deque len (One p1) Empty (One e1) ->
             ( Just e1, Single p1 )
 
-        Deque (Two p1 p2) Empty (One e1) ->
-            ( Just e1, Deque (One p1) Empty (One p2) )
+        Deque len (Two p1 p2) Empty (One e1) ->
+            ( Just e1, Deque (len - 1) (One p1) Empty (One p2) )
 
-        Deque (Three p1 p2 p3) Empty (One e1) ->
-            ( Just e1, Deque (Two p1 p2) Empty (One p3) )
+        Deque len (Three p1 p2 p3) Empty (One e1) ->
+            ( Just e1, Deque (len - 1) (Two p1 p2) Empty (One p3) )
 
-        Deque (Four p1 p2 p3 p4) Empty (One e1) ->
-            ( Just e1, Deque (Two p1 p2) Empty (Two p3 p4) )
+        Deque len (Four p1 p2 p3 p4) Empty (One e1) ->
+            ( Just e1, Deque (len - 1) (Two p1 p2) Empty (Two p3 p4) )
 
-        Deque (Five p1 p2 p3 p4 p5) Empty (One e1) ->
-            ( Just e1, Deque (Four p1 p2 p3 p4) Empty (One p5) )
+        Deque len (Five p1 p2 p3 p4 p5) Empty (One e1) ->
+            ( Just e1, Deque (len - 1) (Four p1 p2 p3 p4) Empty (One p5) )
 
-        Deque (Six p1 p2 p3 p4 p5 p6) Empty (One e1) ->
-            ( Just e1, Deque (Three p1 p2 p3) Empty (Three p4 p5 p6) )
+        Deque len (Six p1 p2 p3 p4 p5 p6) Empty (One e1) ->
+            ( Just e1, Deque (len - 1) (Three p1 p2 p3) Empty (Three p4 p5 p6) )
 
-        Deque beginning middle (One e1) ->
+        Deque len beginning middle (One e1) ->
             let
                 ( newEnd, newMiddle ) =
                     popBufferBack middle
@@ -235,7 +235,7 @@ popBack deque =
                     ( Nothing, Empty )
 
                 Just val ->
-                    ( Just e1, Deque beginning newMiddle val )
+                    ( Just e1, Deque (len - 1) beginning newMiddle val )
 
 
 popBufferBack : Deque (Buffer a) -> ( Maybe (Buffer a), Deque (Buffer a) )
@@ -262,7 +262,7 @@ foldl fn acc deque =
         Single a ->
             fn a acc
 
-        Deque beginning middle end ->
+        Deque _ beginning middle end ->
             bufferFoldl fn end (foldlStep (bufferFoldl fn) (bufferFoldl fn beginning acc) middle)
 
 
@@ -302,7 +302,7 @@ foldr fn acc deque =
         Single a ->
             fn a acc
 
-        Deque beginning middle end ->
+        Deque _ beginning middle end ->
             bufferFoldr fn beginning (foldrStep (bufferFoldr fn) (bufferFoldr fn end acc) middle)
 
 
@@ -335,7 +335,15 @@ bufferFoldr fn buffer acc =
 
 length : Deque a -> Int
 length deque =
-    foldl (\_ acc -> acc + 1) 0 deque
+    case deque of
+        Empty ->
+            0
+
+        Single _ ->
+            1
+
+        Deque len _ _ _ ->
+            len
 
 
 append : Deque a -> Deque a -> Deque a
